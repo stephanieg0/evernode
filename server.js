9e1e05d55b25d9b89b189a49224a0bd77e9e35cf
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-02-18 10:36:02 -0600

    create new note form added

--- server.js
+++ server.js
@@ -2,12 +2,27 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
+//making jade accesible
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 //Root Route
 app.get('/', (req, res) => {
   res.send('Server Running');
+});
+//route to serve up the form
+app.get('/notes/new', (req, res) => {
+  res.render('new-note');
+});
+app.post('/notes', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 //listen on a port
 app.listen(port, () => {
