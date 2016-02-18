b0f44d1a76441dcba16308eaf0cc79af95c3a09f
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-02-18 10:53:28 -0600

    note saves to database

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const app = express();
 const port = process.env.PORT || 3000;
-
+const Note = mongoose.model('Notes', mongoose.Schema({
+  title: String,
+  text: String
+}));
 //making jade accesible
 app.set('view engine', 'jade');
 
@@ -22,9 +26,20 @@ app.get('/notes/new', (req, res) => {
 });
 app.post('/notes', (req, res) => {
   console.log(req.body);
-  res.redirect('/');
+  Note.create(req.body, (err, note) => {
+    if (err) throw err;
+    console.log(note);
+    res.redirect('/');
+  });
 });
-//listen on a port
-app.listen(port, () => {
-  console.log(`Evernode server running on port: ${port}`);
-})
+
+//connecting to mongoose. It has to wrap the port listen function.
+mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
+  if (err) throw err;
+
+  //listen on a port
+  app.listen(port, () => {
+    console.log(`Evernode server running on port: ${port}`);
+  });
+});
+
