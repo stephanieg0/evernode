59362ea50c1271543177b4be5db145db5f5a83de
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-02-18 11:20:04 -0600

    added view route and jade file

--- server.js
+++ server.js
@@ -24,12 +24,20 @@ app.get('/', (req, res) => {
 app.get('/notes/new', (req, res) => {
   res.render('new-note');
 });
+//route to see individual note with a route parameter
+app.get('/notes/:id', (req, res) => {
+  Note.findById(req.params.id, (err, note) => {
+    if (err) throw err;
+    res.render('show-note', {note: note});
+  });
+});
+
 app.post('/notes', (req, res) => {
   console.log(req.body);
   Note.create(req.body, (err, note) => {
     if (err) throw err;
     console.log(note);
-    res.redirect('/');
+    res.redirect(`/notes/${note._id}`);
   });
 });
 
