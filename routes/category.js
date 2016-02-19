5fac1d767fa76430209901133f3029abff40a61c
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-02-19 14:31:34 -0600

    category shows in template

--- routes/category.js
+++ routes/category.js
@@ -9,10 +9,13 @@ const Note = require('../models/note');
 const categoryCtrl = require('../controllers/category');
 
 router.param('id', (req, res, next, id) => {
+  //finding the category id to pass on
   Category.findById(id, (err, category) => {
     if (err) throw err;
-
+    //assigning the category to request to be available
     req.category = category;
+
+    //finding the note that has an equivalent id as the category
     Note.find({category: id}, (err, notes) => {
       if (err) throw err;
       req.category.notes = notes;
